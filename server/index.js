b1db1d8d3162261c12b6052aea9411088331c55a
waldermx <gotreowen@gmail.com>
2023-03-27 12:26:50 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pkg from 'body-parser';
+const { urlencoded } = pkg;
+import mongoose from "mongoose";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js"
+
+
+/* CONFIGURATION */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
+
+/*Routing*/
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes)
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=>{
+    app.listen(PORT, ()=> console.log(`Server Port: ${PORT}`))
+}).catch((error)=>console.log(error, "did not connect"))
