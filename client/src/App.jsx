cc1330fef6790e4826497a2a79250e09c0dac4d0
waldermx <gotreowen@gmail.com>
2023-03-29 10:41:08 -0600

    App design (routing, outlet, css)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { useSelector } from 'react-redux';
+import { themeSettings } from './theme';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Dashboard from "./scenes/dashboard";
+import Layout from "./scenes/layout"
 
-export default function App() {
+
+
+function App() {
+  const mode = useSelector((state)=>state.global.mode);
+  const theme = useMemo(()=>createTheme(themeSettings(mode)), [mode])
   return (
     <div className='app'>
-      
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>
+          <Routes>
+            <Route element={<Layout/>}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />}/>
+              <Route path="/dashboard" element={<Dashboard/>}/>
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
+
+export default App;
